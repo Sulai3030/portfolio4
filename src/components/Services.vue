98f131e0cd6a5cb240674619908096fd68f19fc8
sully34 <sssulai@gmail.com>
2021-02-03 18:29:41 -0500

    updated Clients section

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -24,8 +24,8 @@
       >
         <v-row
           class="fill-height"
-          align="center"
-          justify="center"
+          align="left"
+          justify="left"
         >
           <v-col cols="12">
             <base-bubble-2
@@ -36,29 +36,56 @@
             </base-heading>
 
             <base-text class="mb-5">
-              Curabitur venenatis tortor erat, quis laoreet nis<br>
-              lobortis eget. Fusce tempor aucto.
+              <p class="text-left">BlackHealthMatters.com <a href="https://blackhealthmatters.com/" target="_blank">(https://blackhealthmatters.com)</a>     New York, NY 
+                <ul>            
+                  <li>Write Health Articles targeted to an African American audience					2019 - Present</li>	
+                <li>
+                  <a href="https://blackhealthmatters.com/lauren-lee-talks-kidney-disease-breakthroughs/" target="_blank">https://blackhealthmatters.com/lauren-lee-talks-kidney-disease-breakthroughs/</a>
+                </li>	
+                <li>
+                  <a href="https://blackhealthmatters.com/working-with-your-doctor-when-you-have-kidney-disease-empowering-yourself-to-be-an-advocate-for-you/" target="_blank">https://blackhealthmatters.com/working-with-your-doctor-when-you-have-kidney-disease-empowering-yourself-to-be-an-advocate-for-you/</a></li>
+                </ul>
+              </p>
+
+            </base-text>
+            <base-text class="mb-5">
+              <p class="text-left">Examiner.com <a href="https://blackhealthmatters.com/" target="_blank"></a>        New York, NY 
+                <ul>            
+                  <li>New York City Budget Meals Examiner </li>	
+                <li>
+                  Wrote, cooked and tested new recipes for Examiner.com
+                </li>	
+                <li>
+                  Regularly manage social media platforms to advertise web content</li>
+                </ul>
+              </p>
+            </base-text>
+            <base-text class="mb-5">
+              <p class="text-left">City of New York Campaign Finance Board     New York, NY
+                Website Copywriter/Assistant to Director of Communications                                                                                        
+                <a href="http://www.nyccfb.info/" target="_blank">http://www.nyccfb.info/</a>     New York, NY 
+                <ul>            
+                  <li>Revamped website. </li>	
+                <li>
+                  Rewrote and proofread 30 + pages editorial copy in a Drupal-based CMS. 
+                </li>	
+                <li>
+                  Collaborated with the Communications Department staff to maintain and update public website 
+                  prior to re-launch. Ensured coherency of portal’s copy.
+                  </li>
+                </ul>
+              </p>
+
             </base-text>
           </v-col>
-
+          
           <v-col
             v-for="(service, i) in services"
             :key="i"
             class="text-center mb-3"
             md="6"
           >
-            <v-avatar
-              class="elevation-6 mb-2"
-              color="#69A1BB"
-              size="64"
-              tile
-            >
-              <v-icon
-                dark
-                size="52"
-                v-text="service.icon"
-              />
-            </v-avatar>
+            
 
             <base-text>
               <div
