60b9804398a528fe2a481125d6d40ab58417dbf0
sully34 <sssulai@gmail.com>
2021-03-22 23:04:12 +0000

    added cards

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -318,16 +318,22 @@
                                 color="#385F73"
                                 dark
                               >
-                                <v-card-title class="headline">
-                                  Unlimited music now
-                                </v-card-title>
+                                <v-card-title class="headline" />
 
-                                <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+                                <v-card-subtitle>
+                                  <p class="text-left">
+                                    Textbook Writer
+                                  </p>
+                                  <p class="text-left">
+                                    Assisted Executive Editor for English Composition and Literature by researching, proofing and writing supplemental materials for an ‘Introduction Level English Textbook’ aimed at state college market and which specifically targeted non-English majors wanting to develop the AP grammar style.
+                                  </p>
+                                  <p class="text-left">
+                                    Edited and proofread educational materials to ensure accuracy and consistency of information. Designed and formulated writing exercises and study questions (20+) for each chapter in text and online versions.
+                                  </p>
+                                </v-card-subtitle>
 
                                 <v-card-actions>
-                                  <v-btn text>
-                                    Listen Now
-                                  </v-btn>
+                                  <v-btn text />
                                 </v-card-actions>
                               </v-card>
                             </v-col>
@@ -418,7 +424,114 @@
               </v-container>
             </v-card>
           </v-col>
-        
+          <v-hover />
+          <v-card
+            max-width="400"
+            class="mx-auto"
+          >
+            <v-system-bar
+              color="pink darken-2"
+              dark
+            >
+              <v-spacer />
+
+              <v-icon>mdi-window-minimize</v-icon>
+
+              <v-icon>mdi-window-maximize</v-icon>
+
+              <v-icon>mdi-close</v-icon>
+            </v-system-bar>
+
+            <v-app-bar
+              dark
+              color="pink"
+            >
+              <v-app-bar-nav-icon />
+
+              <v-toolbar-title>ScreenRant.com</v-toolbar-title>
+
+              <v-spacer />
+
+              <v-btn icon />
+            </v-app-bar>
+            <v-container>
+              <v-row dense>
+                <v-col cols="12">
+                  <v-card
+                    color="#385F73"
+                    dark
+                  >
+                    <v-card-title class="headline" />
+                    <v-card-subtitle>
+                      <p class="text-left">                                                                                        
+                        <a
+                          href="https://screenrant.com/author/sulai/"
+                          target="_blank"
+                        >ScreenRant.com</a> 
+                      </p><p class="text-left">
+                        Wrote multiple daily posts of varying length about upcoming Hollywood movies on deadline
+                        <ul />
+                      </p>
+                    </v-card-subtitle>
+                    <v-card-actions>
+                      <v-btn text />
+                    </v-card-actions>
+                  </v-card>
+                </v-col>
+                <v-col
+                  v-for="(item, i) in items"
+                  :key="i"
+                  cols="12"
+                >
+                  <v-card
+                    :color="item.color"
+                    dark
+                  >
+                    <div class="d-flex flex-no-wrap justify-space-between">
+                      <div>
+                        <v-card-title
+                          class="headline"
+                          v-text="item.title"
+                        />
+
+                        <v-card-subtitle v-text="item.artist" />
+
+                        <v-card-actions>
+                          <v-btn
+                            v-if="item.artist === 'Ellie Goulding'"
+                            class="ml-2 mt-3"
+                            fab
+                            icon
+                            height="40px"
+                            right
+                            width="40px"
+                          >
+                            <v-icon>mdi-play</v-icon>
+                          </v-btn>
+
+                          <v-btn
+                            v-else
+                            class="ml-2 mt-5"
+                            outlined
+                            rounded
+                            small
+                          />
+                        </v-card-actions>
+                      </div>
+
+                      <v-avatar
+                        class="ma-3"
+                        size="125"
+                        tile
+                      >
+                        <v-img :src="item.src" />
+                      </v-avatar>
+                    </div>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
           <v-hover>
             <v-card
               elevation="24"
@@ -426,54 +539,7 @@
               shaped
               max-width="600"
               class="mx-auto"
-            >
-              <p class="text-left">
-                Pearson Education Group   New York, NY
-              </p>                                                                                
-              <a
-                href="http://www.nyccfb.info/"
-                target="_blank"
-              >http://www.nyccfb.info/</a>     New York, NY 
-              <p class="text-left">
-                Textbook Writer
-                <ul>    
-                  <li>
-                    Assisted Executive Editor for English Composition and Literature by researching, proofing and writing
-                    supplemental materials for an ‘Introduction Level English Textbook’ aimed at state college market and which 
-                    specifically targeted non-English majors wanting to develop the AP grammar style. 
-                  </li>	
-                  <li>
-                    Edited and proofread educational materials to ensure accuracy and consistency of information. Designed 
-                    and formulated writing exercises and study questions (20+) for each chapter in text and online versions.
-                  </li>	
-                </ul>
-              </p>
-            </v-card>
-          </v-hover>
-          <v-hover>
-            <v-card
-              elevation="24"
-              outlined
-              shaped
-              max-width="600"
-              class="mx-auto"
-            >
-              <v-card-text> 
-                <p class="text-left">                                                                                        
-                  <a
-                    href="https://screenrant.com/author/sulai/)/"
-                    target="_blank"
-                  >ScreenRant.com</a> 
-                </p><p class="text-left">
-                  Textbook Writer
-                  <ul>            
-                    <li>
-                      Wrote multiple daily posts of varying length about upcoming Hollywood movies on deadline
-                    </li>	
-                  </ul>
-                </p>
-              </v-card-text>
-            </v-card>
+            />
           </v-hover>
         </v-row>
       </v-col>
@@ -526,8 +592,10 @@
   
   }
 .a{
-  color: #000;
+  color: #F7C72A;
 }
-
+.v-application a{
+  color: #F7C72A;
+}
 </style>
 
